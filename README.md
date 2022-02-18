a4c81da7e8cb724f53a7d050e052b30aafc18088
Gualtix <walkter@gmail.com>
2022-02-18 13:00:39 -0600

    Added Ignore

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # BookBorrowing
+
+## Relational Diagram
+![](./img/2022-02-18-11-19-02.png)
+
+
+## Class Diagram
+![](./img/2022-02-18-12-43-54.png)
+
+
+## Trello Board
+https://trello.com/b/avKyN8mS/bookborrowing
+
+
+## Spring Initializr
+![](./img/2022-02-18-12-55-50.png)
---
+++ backend/teluslibrary/src/main/java/com/appllication/teluslibrary/TeluslibraryApplication.java
@@ -0,0 +1,13 @@
+package com.appllication.teluslibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeluslibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeluslibraryApplication.class, args);
+	}
+
+}
