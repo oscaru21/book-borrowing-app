9efd8122b796f6af79d910ad8b75fd41e51a14d1
Oscar Umaña <oscarumana1021@gmail.com>
2022-02-22 11:32:21 -0600

    Deleted duplicated packages

--- backend/teluslibrary/src/main/java/com/appllication/teluslibrary/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.appllication.teluslibrary.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.appllication.teluslibrary.Entity.User;
-
-public interface UserRepository extends CrudRepository<User, Long> 
-{
-	
-}
