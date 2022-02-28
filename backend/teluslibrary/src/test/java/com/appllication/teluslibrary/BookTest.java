526366c2b07efbf674ba6cad2c86f88fc8662b21
Gualtix <walkter@gmail.com>
2022-02-28 17:27:00 -0600

    command

--- backend/teluslibrary/src/test/java/com/appllication/teluslibrary/BookTest.java
+++ backend/teluslibrary/src/test/java/com/appllication/teluslibrary/BookTest.java
@@ -6,6 +6,7 @@ import static org.mockito.Mockito.mock;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import com.appllication.teluslibrary.controllers.BookController;
@@ -50,8 +51,9 @@ public class BookTest {
 	@Autowired
 	private BookController bookController;
 
+    //Get Book by ID
     @Test
-	void getBookController() {
+	void getBook() {
 
         Book book = new Book();
         book.setId(1L);
@@ -59,10 +61,59 @@ public class BookTest {
         book.setStock(1);
 
         BookService bookServiceMock = mock(BookService.class);
-        Mockito.when(bookServiceMock.getBook("TestBook")).thenReturn(book);
+        Mockito.when(bookServiceMock.getBook(1L)).thenReturn(book);
 
         Book tmp_book = bookController.getBook(1L).getBody();
 
-        MatcherAssert.assertThat(tmp_book.getTitle(), equalTo("TestBook"));
+        MatcherAssert.assertThat(tmp_book.getId(), equalTo(1L));
 	}
+
+    //Get Book by Title
+    @Test
+    void getBookByTitle(){
+
+        Book book = new Book();
+        book.setId(1L);
+        book.setTitle("Parasomnia");
+        book.setStock(1);
+
+        BookService bookServiceMock = mock(BookService.class);
+        Mockito.when(bookServiceMock.getBook("Parasomnia")).thenReturn(book);
+
+        Book tmp_book = bookController.getBookByTitle("Parasomnia").getBody();
+
+        MatcherAssert.assertThat(tmp_book.getTitle(), equalTo("Parasomnia"));
+    }
+
+    //Get all books
+    @Test
+    void getAllBooks(){
+
+        List<Book> Catalog = bookRepository.findAll();
+
+        BookService bookServiceMock = mock(BookService.class);
+        Mockito.when(bookServiceMock.getBook()).thenReturn(new ArrayList<Book>());
+
+        List<Book> tmp_book = bookController.getAllBooks();
+
+        MatcherAssert.assertThat(tmp_book.size(), equalTo(Catalog.size()));
+
+    }
+
+    //Create book
+    @Test
+    void createBook(){
+
+        Book book = new Book();
+        book.setId(1L);
+        book.setTitle("TestBook");
+        book.setStock(1);
+
+        BookService bookServiceMock = mock(BookService.class);
+        Mockito.when(bookServiceMock.createBook(book)).thenReturn(book);
+
+        Book tmp_book = bookController.createBook(book).getBody();
+
+        MatcherAssert.assertThat(tmp_book.getId(), equalTo(1L));
+    }
 }
